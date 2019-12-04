510f1dbe03e4397fced74fec0fc2f16f4926beed
JiaXing <s3a632055@gm.student.ncut.edu.tw>
2019-12-04 17:04:25 +0800

    aa.a2

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/aa/a1', function () {
     return view('aa.a1');
 });
+Route::get('/aa/a2', function () {
+    return view('aa.a2');
+});
